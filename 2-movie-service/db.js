7f37316d8a7d82695af8f681be3fa3efe4579d16
Nara Naurah <sittinaurah@gmail.com>
2026-06-15 22:36:57 +0700

    benerin kek apalah banyak bgt pokoknya

--- 2-movie-service/db.js
+++ 2-movie-service/db.js
@@ -1,12 +1,14 @@
+module.exports = db;
+
 const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'nontonin_movies',
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect(err => {
@@ -16,5 +18,3 @@ db.connect(err => {
     console.log('Connected to MySQL');
   }
 });
-
-module.exports = db;
